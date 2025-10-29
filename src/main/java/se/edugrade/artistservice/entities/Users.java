fe3edc2b12a3259201f5cc523ae50f737c23b909
Jocelyn <aracely.campos@hotmail.com>
2025-10-29 17:36:15 +0100

    3 entities done

--- src/main/java/se/edugrade/artistservice/entities/Users.java
+++ src/main/java/se/edugrade/artistservice/entities/Users.java
@@ -19,6 +19,50 @@ public class Users {
     @Column(name = "email", length = 100, nullable = false, unique = true)
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 
 }
 
