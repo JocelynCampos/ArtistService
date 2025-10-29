495f14e5c4e1675c48bfd6bcdea82bbe29fe30ed
Jocelyn <aracely.campos@hotmail.com>
2025-10-29 17:05:24 +0100

    third commit

--- src/main/java/se/edugrade/artistservice/entities/Users.java
+++ src/main/java/se/edugrade/artistservice/entities/Users.java
@@ -1,4 +1,25 @@
 package se.edugrade.artistservice.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "userName", nullable = false, length = 50)
+    private String userName;
+
+    @Column(name = "password", length = 50, nullable = false)
+    private String password;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+
 }
+
+
